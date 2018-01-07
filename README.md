58a4743387dcae2e2505a74f60670bda088ac010
Eric MULLER <e.mul@free.fr>
2018-01-07 17:21:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+##SpringBoot BOM Test
+  
+    
+```
+<parent>
+<groupId>com.emu.apps.starter</groupId>
+<artifactId>spring-boot-starter-bom</artifactId>
+<version>0.0.1-SNAPSHOT</version>
+ </parent>
+ ```
---
+++ src/main/java/com/emu/apps/starter/test/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.emu.apps.starter.test.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
